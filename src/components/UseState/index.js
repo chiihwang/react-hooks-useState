a5a6d678e6c13bb3ba8524628b1ea7f8a047835d
chiihwang <38438241+chiihwang@users.noreply.github.com>
2023-03-01 14:19:53 -0500

    v1

--- src/components/UseState/index.js
+++ src/components/UseState/index.js
@@ -1,7 +1,27 @@
+import "./UseState.css";
+import { useState } from "react";
+
 const UseState = () => {
+  const [theme, setTheme] = useState("light");
+  const [count, setCount] = useState(window.localStorage.getItem("item") || 0);
+
   return (
-    <div>
+    <div className={theme}>
       <h1>UseState Component</h1>
+      {/*<button onClick={() => setTheme("dark")}>Dark</button>*/}
+      {/*<button onClick={() => setTheme("light")}>Light</button>*/}
+      <button
+        onClick={() =>
+          theme === "dark" ? setTheme("light") : setTheme("dark")
+        }
+      >
+        Toggle Theme
+      </button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
     </div>
   );
 };
